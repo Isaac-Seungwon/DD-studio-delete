e96f868717fc8a7670743aad37d4423b7a7886bd
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-11-03 16:04:55 +0900

    init

--- src/main/java/com/ddstudio/index.java
+++ src/main/java/com/ddstudio/index.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/name.do")
+@WebServlet("/index.do")
 public class index extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/name.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/index.jsp");
 		dispatcher.forward(req, resp);
 	}
 
